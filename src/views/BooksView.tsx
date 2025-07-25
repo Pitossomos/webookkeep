d1e7458cb8d9885762a7ebe22f6a8b04f0aa1441
Pitossomo <pedro.h.teles@hotmail.com>
2025-07-25 18:10:40 -0300

    feat/BooksView: Remove delete button and improve responsiveness

--- src/views/BooksView.tsx
+++ src/views/BooksView.tsx
@@ -1,4 +1,4 @@
-import { Book, Edit, Eye, Plus, Trash2 } from "lucide-react";
+import { Book, Edit, Eye, Plus, Trash2, Wallet } from "lucide-react";
 import MainWrapper from "../components/MainWrapper";
 import TitleWrapper from "../components/TitleWrapper";
 import PlusButton from "../components/PlusButton";
@@ -22,29 +22,29 @@ const BooksView = () => {
       {books.length === 0 ? (
         <p className="text-gray-600 text-lg">Nenhum livro cadastrado. Comece criando um novo livro!</p>
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
           {books.map((book) => (
             <div key={book.id} className="bg-white p-6 rounded-lg shadow-lg border border-gray-200">
               <h3 className="text-xl font-semibold mb-2 text-gray-800">{book.name}</h3>
               <p className="text-gray-600 mb-4 text-sm">{book.description || 'Sem descrição.'}</p>
-              <div className="flex space-x-3">
+              <div className="flex flex-wrap">
                 <button
-                  className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600 transition-colors duration-200 flex items-center text-sm"
+                  className="px-4 py-2 m-1 bg-teal-500 text-white rounded-md hover:bg-teal-600 transition-colors duration-200 flex items-center text-sm"
                 >
                   <Eye size={16} className="mr-1" />
+                  Ver Transações
+                </button>
+                <button
+                  className="px-4 py-2 m-1 bg-purple-500 text-white rounded-md hover:bg-purple-600 transition-colors duration-200 flex items-center text-sm"
+                >
+                  <Wallet size={16} className="mr-1" />
                   Ver Contas
                 </button>
                 <button
-                  className="px-4 py-2 bg-yellow-500 text-white rounded-md hover:bg-yellow-600 transition-colors duration-200 flex items-center text-sm"
+                  className="px-4 py-2 m-1 bg-yellow-500 text-white rounded-md hover:bg-yellow-600 transition-colors duration-200 flex items-center text-sm"
                 >
                   <Edit size={16} className="mr-1" />
-                  Editar
-                </button>
-                <button
-                  className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 transition-colors duration-200 flex items-center text-sm"
-                >
-                  <Trash2 size={16} className="mr-1" />
-                  Excluir
+                  Editar Livro
                 </button>
               </div>
             </div>
