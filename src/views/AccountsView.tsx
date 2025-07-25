e42dcc2c932055c16c8f399d34ce44809b4d323e
Pitossomo <pedro.h.teles@hotmail.com>
2025-07-25 18:08:47 -0300

    feat/AccountsView: Simplify account list

--- src/views/AccountsView.tsx
+++ src/views/AccountsView.tsx
@@ -27,29 +27,18 @@ const AccountsView = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {accounts.map((account) => (
             <div key={account.id} className="bg-white p-6 rounded-lg shadow-lg border border-gray-200">
-              <h3 className="text-xl font-semibold mb-2 text-gray-800">{account.name}</h3>
-              <p className="text-gray-600 mb-2 text-sm">{account.description || 'Sem descrição.'}</p>
-              <p className="text-gray-700 font-medium text-sm mb-4">Tipo: {account.type}</p>
-              <div className="flex space-x-3">
+              <h3 className="text-xl font-semibold mb-2 text-gray-800 flex justify-between">
+                {account.name}
                 <button
-                  className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors duration-200 flex items-center text-sm"
+                  className="p-2 bg-yellow-500 text-white left rounded-md hover:bg-yellow-600 transition-colors duration-200"
                 >
-                  <Repeat size={16} className="mr-1" />
-                  Ver Transações
-                </button>
-                <button
-                  className="px-4 py-2 bg-yellow-500 text-white rounded-md hover:bg-yellow-600 transition-colors duration-200 flex items-center text-sm"
-                >
-                  <Edit size={16} className="mr-1" />
-                  Editar
-                </button>
-                <button
-                  className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 transition-colors duration-200 flex items-center text-sm"
-                >
-                  <Trash2 size={16} className="mr-1" />
-                  Excluir
-                </button>
-              </div>
+                  <Edit size={16} />
+                </button>  
+              </h3>
+              <p className="text-gray-700 font-medium text-sm mb-4">
+                Tipo: {account.type}
+              </p>  
+              <p className="text-gray-600 mb-2 text-sm">{account.description}</p>
             </div>
           ))}
         </div>
