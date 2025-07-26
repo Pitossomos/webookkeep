d3189fe17b4b7f3f67f45b411ea7b87711a98479
Pitossomo <pedro.h.teles@hotmail.com>
2025-07-26 18:39:10 -0300

    feat/PlusButton: Remove conditional color option

--- src/components/PlusButton.tsx
+++ src/components/PlusButton.tsx
@@ -4,14 +4,13 @@ import type { ReactNode } from "react";
 type PlusButtonProps = {
   children: ReactNode,
   onClick?: () => {},
-  pageThemeColor?: string,
 }
 
-const PlusButton = ({ onClick, children, pageThemeColor = "blue" }: PlusButtonProps) => {
+const PlusButton = ({ children }: PlusButtonProps) => {
   return (
     <div className="flex justify-between items-center mb-6">
       <button
-        className={`px-6 py-3 bg-${pageThemeColor}-600 text-white rounded-lg shadow-md hover:bg-${pageThemeColor}-700 transition-colors duration-200 flex items-center`}
+        className={`px-6 py-3 bg-blue-600 text-white flex items-center rounded-lg shadow-md hover:bg-blue-700 transition-colors duration-200`}
       >
         <Plus size={20} className="mr-2" />
         { children }
