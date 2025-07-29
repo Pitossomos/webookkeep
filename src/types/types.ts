7ece7db0d9b52b762f4f851a6a633cd66033bd4a
Pitossomo <pedro.h.teles@hotmail.com>
2025-07-29 06:05:22 -0300

    feat/types: Add types for maisn business entities

---
+++ src/types/types.ts
@@ -0,0 +1,30 @@
+export type Transaction = {
+  id: number;
+  bookId: number;
+  source_account_id: number;
+  destination_account_id: number;
+  datetime: string;
+  description: string;
+  value: number;
+};
+
+export type Account = {
+  id: number;
+  bookId: number;
+  name: string;
+  description?: string;
+  type: 'Asset' | 'Liability' | 'Income' | 'Expense' | 'Equity';
+};
+
+export type Book = {
+  id: number;
+  name: string;
+  description?: string;
+};
+
+export type User = {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+};
