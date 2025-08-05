93fd8c5da5becf6305553252b017fccf44b885cd
Pitossomos <pedro.h.teles@hotmail.com>
2025-08-05 18:16:19 -0300

    chore/AccountsView: Access data from mock

--- src/views/AccountsView.tsx
+++ src/views/AccountsView.tsx
@@ -3,15 +3,10 @@ import MainWrapper from "../components/MainWrapper";
 import TitleWrapper from "../components/TitleWrapper";
 import PlusButton from "../components/PlusButton";
 import Accounts from "../components/Accounts";
-import type { Account } from "../types/types";
+import { ACCOUNTS as accounts, BOOKS } from "../data/data";
 
 const AccountsView = () => {
-  const accounts: Account[] = [
-    { id: 1, bookId: 1, name: 'Conta 1', description: 'Descrição da Conta 1', type: 'Asset' },
-    { id: 2, bookId: 1, name: 'Conta 2', description: 'Descrição da Conta 2', type: 'Income' },
-  ]
-
-  const book = { id: 1, name: 'Livro 1' };
+  const book = BOOKS[0];
 
   return (
     <MainWrapper>
@@ -19,14 +14,11 @@ const AccountsView = () => {
         <Wallet className="mr-3 text-purple-600" size={32} />
         Contas para "{book.name}"
       </TitleWrapper>
-      <PlusButton>
-        Criar Nova Conta
-      </PlusButton>
+      <PlusButton>Criar Nova Conta</PlusButton>
 
       <Accounts accounts={accounts} />
-
     </MainWrapper>
   );
 };
 
-export default AccountsView
+export default AccountsView;
