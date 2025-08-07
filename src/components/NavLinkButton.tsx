9f573890517b65cca340d28a0d1c14602c2a9984
Pitossomos <pedro.h.teles@hotmail.com>
2025-08-07 06:31:12 -0300

    style/NavLink: move text styles on parent

--- src/components/NavLinkButton.tsx
+++ src/components/NavLinkButton.tsx
@@ -1,13 +1,16 @@
 import { NavLink } from "react-router";
 
 type NavLinkButtonProps = {
-  to: string,
+  to: string;
   children: React.ReactNode;
-}
+};
 
 const NavLinkButton = ({ to, children }: NavLinkButtonProps) => {
   return (
-    <NavLink className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white" to={to}>
+    <NavLink
+      className="rounded-md px-3 py-2 hover:bg-gray-700 hover:text-white"
+      to={to}
+    >
       {children}
     </NavLink>
   );
