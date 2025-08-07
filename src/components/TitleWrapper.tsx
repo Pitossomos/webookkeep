4b4c0a2994a8618e5a31d017b16f979a9e9c6212
Pitossomos <pedro.h.teles@hotmail.com>
2025-08-07 06:30:40 -0300

    fix/TitleWrapper: Change h1 for div

--- src/components/TitleWrapper.tsx
+++ src/components/TitleWrapper.tsx
@@ -1,11 +1,11 @@
-import type { ReactNode } from "react"
+import type { ReactNode } from "react";
 
 const TitleWrapper = ({ children }: { children: ReactNode }) => {
   return (
-    <h1 className="text-3xl font-extrabold mb-6 text-gray-900 flex items-center">
-      { children }
-    </h1>
-  )
-}
+    <div className="text-3xl font-extrabold mb-6 text-gray-900 flex items-center">
+      {children}
+    </div>
+  );
+};
 
 export default TitleWrapper;
