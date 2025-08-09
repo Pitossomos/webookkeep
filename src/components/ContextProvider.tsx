b8c7c355601c1adb7908c1883e703540edadb100
Pitossomos <pedro.h.teles@hotmail.com>
2025-08-09 09:01:21 -0300

    feat/ContextProvider: Add setContext property

--- src/components/ContextProvider.tsx
+++ src/components/ContextProvider.tsx
@@ -1,16 +1,18 @@
 import { createContext } from "react";
 import type { Account, Book, Transaction } from "../types/types";
 
-type ContextType = {
+export type ContextType = {
   book: Book | null;
   accounts: Account[];
   transactions: Transaction[];
+  setContext: null | ((context: ContextType) => void);
 };
 
 const ContextProvider = createContext<ContextType>({
   book: null,
   accounts: [],
   transactions: [],
+  setContext: () => {},
 });
 
 export default ContextProvider;
