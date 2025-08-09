a3af42f4727314ed25a3b2b53069b9e8105d464f
Pitossomos <pedro.h.teles@hotmail.com>
2025-08-09 09:03:59 -0300

    feat/App.tsx: Remove ContextProvider (for now, until backend is ready)

--- src/App.tsx
+++ src/App.tsx
@@ -6,43 +6,37 @@ import AccountEditView from "./views/AccountEditView";
 import BookEditView from "./views/BookEditView";
 import BookView from "./views/BookView";
 import NavBar from "./components/NavBar";
-import ContextProvider from "./components/ContextProvider";
 import NotFound from "./components/NotFound";
 
 function App() {
   return (
-    <ContextProvider value={{ accounts: [], book: null, transactions: [] }}>
-      <BrowserRouter>
-        <NavBar />
+    <BrowserRouter>
+      <NavBar />
 
-        <Routes>
-          <Route index element={<h1>Hi</h1>} />
-          <Route path="books">
-            <Route index element={<BooksView />} />
+      <Routes>
+        <Route index element={<h1>Hi</h1>} />
+        <Route path="books">
+          <Route index element={<BooksView />} />
 
-            <Route path=":bookId">
-              <Route index element={<BookView />} />
-              <Route path="edit" element={<BookEditView editing={true} />} />
-              <Route path="transactions" element={<TransactionsView />} />
-              <Route path="accounts">
-                <Route index element={<AccountsView />} />
-                <Route
-                  path=":accountId/edit"
-                  element={<AccountEditView editing={true} />}
-                />
-                <Route
-                  path="new"
-                  element={<AccountEditView editing={false} />}
-                />
-              </Route>
+          <Route path=":bookId">
+            <Route index element={<BookView />} />
+            <Route path="edit" element={<BookEditView editing={true} />} />
+            <Route path="transactions" element={<TransactionsView />} />
+            <Route path="accounts">
+              <Route index element={<AccountsView />} />
+              <Route
+                path=":accountId/edit"
+                element={<AccountEditView editing={true} />}
+              />
+              <Route path="new" element={<AccountEditView editing={false} />} />
             </Route>
-            <Route path="new" element={<BookEditView editing={false} />} />
           </Route>
+          <Route path="new" element={<BookEditView editing={false} />} />
+        </Route>
 
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </ContextProvider>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
